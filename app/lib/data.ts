dd6f5a2d96051e312585ed1e64a68baf1edecfb3
Maximiliano Duran <maximiliano.duran114@gmail.com>
2024-12-31 14:11:50 -0300

    commit 3

---
+++ app/lib/data.ts
@@ -0,0 +1,223 @@
+export interface Item {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export const data: Item[] = [
+  {
+    id: 1,
+    name: "Javascript",
+
+    image: "/javascript.png",
+  },
+  {
+    id: 2,
+    name: "Python",
+
+    image: "/python.png",
+  },
+  {
+    id: 3,
+    name: "Java",
+
+    image: "/Java.png",
+  },
+  {
+    id: 4,
+    name: "Typescript",
+
+    image: "/typescript.png",
+  },
+  {
+    id: 5,
+    name: "Html",
+
+    image: "/html.png",
+  },
+  {
+    id: 6,
+    name: "Css",
+
+    image: "/css.png",
+  },
+  {
+    id: 7,
+    name: "SQL",
+
+    image: "/sql.png",
+  },
+];
+
+export const data2: Item[] = [
+  {
+    id: 1,
+    name: "React",
+
+    image: "/react.png",
+  },
+  {
+    id: 2,
+    name: "Ionic",
+
+    image: "/ionic.png",
+  },
+  {
+    id: 3,
+    name: "Angular",
+
+    image: "/angular.png",
+  },
+  {
+    id: 4,
+    name: "Tailwind",
+
+    image: "/tailwin.png",
+  },
+  {
+    id: 5,
+    name: "Django",
+
+    image: "/django.png",
+  },
+  {
+    id: 6,
+    name: "jasmine",
+
+    image: "/jasmine.png",
+  },
+  {
+    id: 7,
+    name: "daisyui",
+
+    image: "/daisyui.webp",
+  },
+  {
+    id: 8,
+    name: "nextui",
+
+    image: "/nextui.png",
+  },
+  {
+    id: 9,
+    name: "chartjs",
+
+    image: "/chartjs.png",
+  },
+  {
+    id: 10,
+    name: "cypress",
+
+    image: "/cypress.png",
+  },
+  {
+    id: 11,
+    name: "nextjs",
+
+    image: "/nextjs.png",
+  },
+];
+
+export const data3: Item[] = [
+  {
+    id: 1,
+    name: "Firebase",
+
+    image: "/fireb.png",
+  },
+  {
+    id: 2,
+    name: "Mysql",
+
+    image: "/mysql.png",
+  },
+  {
+    id: 3,
+    name: "Vercel",
+
+    image: "/vercel.png",
+  },
+  {
+    id: 4,
+    name: "Oraclesql",
+
+    image: "/oraclesql.png",
+  },
+];
+export const data4: Item[] = [
+  {
+    id: 1,
+    name: "git",
+
+    image: "/git.png",
+  },
+  {
+    id: 2,
+    name: "github",
+
+    image: "/github.png",
+  },
+  {
+    id: 3,
+    name: "vscode",
+
+    image: "/vscode.png",
+  },
+  {
+    id: 4,
+    name: "notion",
+
+    image: "/notion.png",
+  },
+  {
+    id: 5,
+    name: "excel",
+
+    image: "/excel.png",
+  },
+  {
+    id: 6,
+    name: "intellijidea",
+
+    image: "/intellijidea.png",
+  },
+];
+
+export const proyects: Item[] = [
+  {
+    id: 1,
+    name: "Web de juegos",
+
+    image: "",
+  },
+  {
+    id: 2,
+    name: "Apuntes para estudiantes",
+
+    image: "",
+  },
+  {
+    id: 3,
+    name: "Dulce armonia",
+
+    image: "",
+  },
+  {
+    id: 4,
+    name: "Snake Game",
+
+    image: "",
+  },
+  {
+    id: 5,
+    name: "Landing page",
+
+    image: "",
+  },
+  {
+    id: 6,
+    name: "Proximamente",
+
+    image: "",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,116 @@
 import Image from "next/image";
-
+import { data, proyects } from "./lib/data";
+import { data2 } from "./lib/data";
+import { data3 } from "./lib/data";
+import { data4 } from "./lib/data";
 export default function Home() {
   return (
-    <div className="bg-red-400 w-full h-96 flex  ">
-      <div className="w-[50%] bg-blue-300 flex justify-center items-center ">
-        <Image
-          src="/coding.png"
-          width={300}
-          height={300}
-          className="rounded-full"
-        />
+    <div>
+      <div className=" w-full h-96 flex   ">
+        <div className="w-[50%]  flex justify-center items-center ">
+          <Image
+            alt=""
+            src="/coding.png"
+            width={300}
+            height={300}
+            className="rounded-full "
+          />
+        </div>
+        <div className="flex flex-col justify-center gap-5  w-[50%] ml-10 ">
+          <h1 className="text-5xl ">Maximiliano Duran</h1>
+          <h3 className="text-2xl">Computer Engineer Student</h3>
+
+          <ul className="list-disc flex flex-col gap-1 ml-5 ">
+            <li>
+              I am a self taught Full Stack Developer and Mobile Developer
+            </li>
+            <li> I am a student of Computer engineer</li>
+            <li>Working on Node.js</li>
+            <li>Main languages: Java, JavaScript</li>
+            <li>Interested in Full Stack Web and Mobile developer</li>
+          </ul>
+        </div>
       </div>
-      <div className="flex flex-col justify-center items-center">
-        <h1>Maximiliano Duran</h1>
-        <h3>Computer Engineer Student</h3>
-        <ul>
-          <li>I am a self taught Full Stack Developer and Mobile Developer</li>
-          <li> I am a student of Computer engineer</li>
-          <li>Working on Node.js</li>
-          <li>Main languages: Java, JavaScript</li>
-          <li>Interested in Full Stack Web and Mobile developer</li>
-        </ul>
+      <div className="flex flex-col gap-10 ">
+        <div className="flex flex-col gap-5  ">
+          <h1 className="text-2xl ml-5">👨‍💻 Programming Languages</h1>
+          <div className="flex  gap-10 ml-5">
+            {data.map((item) => (
+              <div key={item.id} className="flex gap-5 ">
+                <Image
+                  alt=""
+                  src={item.image}
+                  width={70}
+                  height={70}
+                  className="object-scale-down "
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-2xl ml-5">🧰 Frameworks and Libraries</h1>
+          <div className="flex  gap-10 ml-5">
+            {data2.map((item) => (
+              <div key={item.id} className="flex gap-5 ">
+                <Image
+                  alt=""
+                  src={item.image}
+                  width={70}
+                  height={70}
+                  className="object-scale-down "
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-2xl ml-5">🗄️ Databases and Cloud Hosting</h1>
+          <div className="flex  gap-10 ml-5">
+            {data3.map((item) => (
+              <div key={item.id} className=" ">
+                <Image
+                  alt=""
+                  src={item.image}
+                  width={70}
+                  height={70}
+                  className="object-scale-down "
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-2xl ml-5">💻 Software and Tools</h1>
+          <div className="flex  gap-10 ml-5">
+            {data4.map((item) => (
+              <div key={item.id} className="flex gap-5 ">
+                <Image
+                  alt=""
+                  src={item.image}
+                  width={70}
+                  height={70}
+                  className="object-scale-down "
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className=" mb-10">
+          <h1 className="text-2xl ml-5">🛠️ Projects</h1>
+          <div className="flex flex-col items-center">
+            <div className="grid grid-cols-3 grid-rows-2 gap-20">
+              {proyects.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex justify-center items-center border-2 p-20 shadow-xl"
+                >
+                  <div>{item.name}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
